d1cdf64e892861ac74e57bd7078bd3e5e31e6142
Ed Tansey <office.fwd@yahoo.com>
2022-01-07 12:09:31 +0000

    Add shuffleArray function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
+import { fetchQuizQuestions } from './API'
 // Components
 import QuestionCard from './components/QuestionCard'
+// Types
+import { Difficulty } from './API';
 
 const TOTAL_QUESTIONS = 10
 
@@ -11,6 +14,8 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState([])
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(true)
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
   const startTrivia = async () => {}
 
@@ -25,7 +30,14 @@ const App = () => {
       </button>
       <p className='score'>Score: </p>
       <p>Loading Questions...</p>
-      <QuestionCard questionNr={number + 1} totalQuestions={TOTAL_QUESTIONS} />
+      {/* <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback = {checkAnswer}
+      /> */}
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
